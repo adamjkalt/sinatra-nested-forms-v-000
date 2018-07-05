d353c6ce4d3bd1528271edc68a7aef7bafca84de
Adam Kalt <adam.kalt.1@gmail.com>
2018-07-05 00:52:41 +0000

    automatically backed up by learn

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -4,11 +4,11 @@ class Ship
 
   @@ships = []
 
-  def initialize (name, type, booty)
-    @name = name
-    @type = type
-    @booty = booty
-    @@all << self
+  def initialize (args)
+    @name = args[:name]
+    @type = args[:type]
+    @booty = args[:booty]
+    @@ships << self
   end
 
   def self.all
