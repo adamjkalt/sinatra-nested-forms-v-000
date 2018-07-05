6c9e57e393cfe35296559c8568079e8dd7e688da
Adam Kalt <adam.kalt.1@gmail.com>
2018-07-05 00:49:29 +0000

    automatically backed up by learn

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,19 @@
 class Ship
+
+  attr_accessor :name, :type, :booty
+
+  @@all = []
+
+  def initialize (name, type, booty)
+    @name = name
+    @weight = weight
+    @height = height
+    @@all << self
+  end
+
+  def self.all
+  @@all
+  end
+
+
 end
